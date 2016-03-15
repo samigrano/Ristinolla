790609b35bc6d7b2b5a1b082780c975d53860c7b
Sami <satagr@utu.fi>
2016-03-15 12:41:54 +0200

    Menubar lisätty kehykseen.

--- src/Pelilauta/TicTacToeFrame.java
+++ src/Pelilauta/TicTacToeFrame.java
@@ -1,5 +1,6 @@
 package Pelilauta;
 
+import java.awt.BorderLayout;
 import java.awt.FlowLayout;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
@@ -7,6 +8,9 @@ import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 
 public class TicTacToeFrame extends JFrame implements ActionListener {
 	
@@ -31,20 +35,47 @@ public class TicTacToeFrame extends JFrame implements ActionListener {
 		add(saveGame);
 		add(loadGame);
 		
+		//Vaihtoehto napeille, tehd‰ menubar.
+		JMenuBar bar  = new JMenuBar();
+		JMenu file = new JMenu("File");
+		JMenuItem newFile = new JMenuItem("New");
+		JMenuItem save = new JMenuItem("Save");
+		JMenuItem load = new JMenuItem("Load");
+		JMenuItem close = new JMenuItem("Exit");
+		
+		newFile.addActionListener(this);
+		save.addActionListener(this);
+		load.addActionListener(this);
+		close.addActionListener(this);
+		
+		setJMenuBar(bar);
+		bar.add(file);
+		file.add(newFile);
+		file.add(save);
+		file.add(load);
+		file.addSeparator();
+		file.add(close);
+		
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		String name = e.getActionCommand();
 		
+		Pelilauta lauta = new Pelilauta();
+		
 		if(name.equals("New")){
-			System.out.println("Painoit New nappia");
+			lauta.piirraLauta(); // Piirt‰‰ uuden laudan
 		}
 		else if(name.equals("Save")){
 			System.out.println("Painoit Save nappia");
 		}
 		else if(name.equals("Load")){
 			System.out.println("Painoit Load nappia");
+		}
+		else if(name.equals("Exit")){
+			System.out.println("Ohjelma lopetettu");
+			System.exit(0);
 		}
 		
 	}
